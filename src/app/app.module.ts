9c82896abb52d00979568994202a23f32e356e2f
thiagowerneck1990@hotmail.com <thiagowerneck1990@hotmail.com>
2020-07-12 16:54:21 -0300

    Add header and footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/main/header/header.component';
+import { FooterComponent } from './Components/main/footer/footer.component';
+import { LoginComponent } from './Components/login/login.component';
+import { MainComponent } from './Components/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
